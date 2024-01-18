1ec0ca281a81ce3f41344baccc3239bd40826c40
Landit <landit721@gmail.com>
2024-01-18 17:47:39 +0100

    a

--- datuak/gorde.c
+++ datuak/gorde.c
@@ -3,8 +3,25 @@
 #include <stdlib.h>
 
 void idatziFitxategian(char izena[], char testua[]);
+void irakurri(char izena[], char dest[]);
 
 void idatziFitxategian(char izena[], char testua[])
 {
-    SDL_Log("hola");
+
+    char database[300];
+    FILE *fitxategia;
+    irakurri(izena, database);
+    fitxategia = fopen(izena, "w");
+    SDL_strlcat(database, testua, 300);
+    SDL_Log("datuak: %s", database);
+    fprintf(fitxategia, "%s", database);
+    fclose(fitxategia);
+}
+
+void irakurri(char izena[], char dest[])
+{
+    FILE *fitxategia;
+    fitxategia = fopen(izena, "r");
+    fgets(dest, 128, fitxategia);
+    fclose(fitxategia);
 }
--- datuak/gorde.h
+++ datuak/gorde.h
@@ -2,5 +2,6 @@
 #define GORDE_H
 
 void idatziFitxategian(char izena[], char testua[]);
+void irakurri(char izena[], char dest[]);
 
 #endif
